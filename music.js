75ab392e5e872fd54e8f5d34baa379b7496d5f71
tabbytobias2 <108212586+tabbytobias2@users.noreply.github.com>
2023-09-01 10:11:45 +0100

    Update music.js

--- music.js
+++ music.js
@@ -6,10 +6,10 @@ function toggleMusic() {
   if(musicState) {musicState = false;} else {musicState = true;}
   
   if(musicState) {
-    //document.getElementById("audio").play();
+    document.getElementById("audio").play();
     toggleImg.src = "media/music.png";
   } else {
-    //document.getElementById("audio").pause();
+    document.getElementById("audio").pause();
     toggleImg.src = "media/music-off.jpg";
   }
 }
